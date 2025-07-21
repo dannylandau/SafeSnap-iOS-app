55afe124f9a1b073ebe4291119380cb6b917e55c
Marcin Grześkowiak <grzeskowiak.marcin@gmail.com>
2025-07-21 14:43:08 +0200

    Update models

--- SafeSnap/Core/Models/ScanHistoryItem.swift
+++ SafeSnap/Core/Models/ScanHistoryItem.swift
@@ -1,15 +1,39 @@
 //
 //  ScanHistoryItem.swift
-//  SafeScan
+//  SafeSnap
 //
-//  Created by Marcin Grześkowiak on 27/06/2025.
+//  Created by Marcin Grześkowiak on 14/07/2025.
 //
 
 
 import Foundation
+import UIKit
 
-struct ScanHistoryItem: Codable, Identifiable {
-    var id: String { name }
-    let name: String
+struct ScanHistoryItem: Identifiable, Codable, Hashable {
+    let id: UUID
+    let date: Date
+    let product: Product        // assuming your existing Product is Codable
+    let labels: [String]
     let score: Int
+    let thumbnailData: Data?    // small JPG/PNG for list thumbnail
+
+    // Convenience thumbnail image
+    var thumbnail: UIImage? {
+        guard let data = thumbnailData else { return nil }
+        return UIImage(data: data)
+    }
+
+    init(
+        product: Product,
+        labels: [String],
+        score: Int,
+        thumbnail: UIImage? = nil
+    ) {
+        self.id = .init()
+        self.date = .now
+        self.product = product
+        self.labels = labels
+        self.score = score
+        self.thumbnailData = thumbnail?.jpegData(compressionQuality: 0.3)
+    }
 }
