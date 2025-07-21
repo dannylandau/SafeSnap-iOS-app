c412d358df79a422122ea8ecc1ee99014f1ca1ca
Marcin Grześkowiak <grzeskowiak.marcin@gmail.com>
2025-07-21 14:42:42 +0200

    Add blank views for ExportData, Language, Privacy in Settings

---
+++ SafeSnap/Features/Account/Settings/LanguageSettingsView.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageSettingsView.swift
+//  SafeSnap
+//
+//  Created by Marcin Grześkowiak on 14/07/2025.
+//
+
+
+// LanguageSettingsView.swift
+import SwiftUI
+
+struct LanguageSettingsView: View {
+    var body: some View {
+        Text("Language Settings")
+            .navigationTitle("Language")
+    }
+}
---
+++ SafeSnap/Features/Account/Settings/PrivacySettingsView.swift
@@ -0,0 +1,17 @@
+//
+//  PrivacySettingsView.swift
+//  SafeSnap
+//
+//  Created by Marcin Grześkowiak on 14/07/2025.
+//
+
+
+// PrivacySettingsView.swift
+import SwiftUI
+
+struct PrivacySettingsView: View {
+    var body: some View {
+        Text("Privacy Settings")
+            .navigationTitle("Privacy")
+    }
+}
